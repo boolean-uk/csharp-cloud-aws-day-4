8a0653107a27a0533f71b221edb7afa34cbc85e1
AJ <aj.dewilzin@cbos.co.za>
2024-10-15 15:42:11 +0200

    Update class

--- WorkshopBackend/OrderService/Models/Order.cs
+++ WorkshopBackend/OrderService/Models/Order.cs
@@ -11,5 +11,8 @@ namespace InventoryService.Models
         public int OrderId { get; set; }
         public string Product { get; set; }
         public int Quantity { get; set; }
+        public int Amount { get; set; }
+        public bool? Processed { get; set; }
+        public int? Total { get; set; }
     }
 }
