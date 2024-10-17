5b2f6d57d4dbfbaf8d28f1620c5c494ed157c640
Silje Gjesdal Jacobsen <silje.gjesdal.jacobsen@experisacademy.no>
2024-10-17 13:39:48 +0200

    done

--- WorkshopBackend/OrderService/Models/Order.cs
+++ WorkshopBackend/OrderService/Models/Order.cs
@@ -11,7 +11,7 @@ namespace InventoryService.Models
         public int OrderId { get; set; }
         public string Product { get; set; }
         public int Quantity { get; set; }
-        public int Amount { get; set; }
+        public int Amount { get; set; } 
         public bool? Processed { get; set; }
         public int? Total { get; set; }
     }
